4d075b33152a7a2ee60540bd85b492e9dda91167
Jawe <neus2benen@gmail.com>
2015-09-26 19:21:00 +0200

    The big moment

---
+++ main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+	$('#fullpage').fullpage({
+		menu: true,
+		scrollingSpeed: 400,
+		verticalCentered: true,
+		fitToSection: true
+		
+        
+	});
+
+	$(window).trigger('resize');
+
+});
+
